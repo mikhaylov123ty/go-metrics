43628b0345c91d2f5563610cbb0dbab2f29617ca
Alexandr Mikhailov <mikhaylov123ty@gmail.com>
2025-02-19 02:16:23 +0300

    add examples

--- internal/server/api/handlers_test.go
+++ internal/server/api/handlers_test.go
@@ -1,9 +1,14 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http/httptest"
 	"testing"
 
+	"metrics/internal/models"
 	"metrics/internal/storage/memory"
 
 	"github.com/stretchr/testify/assert"
@@ -18,6 +23,20 @@ func init() {
 		dataUpdater: newMemStorage,
 	}
 
+	counterVal := int64(100)
+	gaugeVal := float64(200)
+	newMemStorage.UpdateBatch([]*models.Data{
+		{
+			Type:  "counter",
+			Name:  "testCounter",
+			Delta: &counterVal,
+		},
+		{
+			Type:  "gauge",
+			Name:  "testGauge",
+			Value: &gaugeVal,
+		},
+	})
 }
 
 func TestHandler_Update(t *testing.T) {
@@ -100,4 +119,224 @@ func TestHandler_Update(t *testing.T) {
 			assert.Equal(t, tt.want.code, res.StatusCode, "Codes are not equal")
 		})
 	}
+}
+
+func ExampleHandler_UpdatePostJSON() {
+	// Конструктор структуры тела запроса
+	val := int64(200)
+	data := &models.Data{
+		Type:  "counter",
+		Name:  "testCounter",
+		Delta: &val,
+	}
+
+	// Сериализация в JSON
+	body, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	// Составление запроса с параметрами URL
+	request := httptest.NewRequest(
+		"POST",
+		"http://localhost:8080/update",
+		io.Reader(bytes.NewBuffer(body)))
+	request.Header.Add("Content-Type", "application/json")
+
+	// Создание интерфейса записи
+	w := httptest.NewRecorder()
+
+	// Регистрация нового обработчика
+	handler := NewHandler(commands)
+
+	// Выполнение запроса
+	handler.UpdatePostJSON(w, request)
+
+	// Получение ответа
+	res := w.Result()
+	defer res.Body.Close()
+
+	fmt.Println(res.StatusCode)
+
+	// Output:
+	// 200
+}
+
+func ExampleHandler_UpdatesPostJSON() {
+	// Конструктор структуры тела запроса
+	gaugeVal := float64(200)
+	counterVal := int64(200)
+	data := []*models.Data{
+		{
+			Type:  "gauge",
+			Name:  "testGauge",
+			Value: &gaugeVal,
+		},
+		{
+			Type:  "counter",
+			Name:  "testCounter",
+			Delta: &counterVal,
+		},
+	}
+
+	// Сериализация в JSON
+	body, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	// Составление запроса с параметрами URL
+	request := httptest.NewRequest(
+		"POST",
+		"http://localhost:8080/updates",
+		io.Reader(bytes.NewBuffer(body)))
+	request.Header.Add("Content-Type", "application/json")
+
+	// Создание интерфейса записи
+	w := httptest.NewRecorder()
+
+	// Регистрация нового обработчика
+	handler := NewHandler(commands)
+
+	// Выполнение запроса
+	handler.UpdatesPostJSON(w, request)
+
+	// Получение ответа
+	res := w.Result()
+	defer res.Body.Close()
+
+	fmt.Println(res.StatusCode)
+
+	// Output:
+	// 200
+}
+
+func ExampleHandler_UpdatePost() {
+	// Составление запроса с параметрами URL
+	request := httptest.NewRequest(
+		"POST",
+		"http://localhost:8080/update",
+		nil)
+	request.Header.Add("Content-Type", "text/plain")
+	request.SetPathValue("type", "counter")
+	request.SetPathValue("name", "alloc")
+	request.SetPathValue("value", "3251325234")
+
+	// Создание интерфейса записи
+	w := httptest.NewRecorder()
+
+	// Регистрация нового обработчика
+	handler := NewHandler(commands)
+
+	// Выполнение запроса
+	handler.UpdatePost(w, request)
+
+	// Получение ответа
+	res := w.Result()
+	defer res.Body.Close()
+
+	fmt.Println(res.StatusCode)
+
+	// Output:
+	// 200
+}
+
+func ExampleHandler_ValueGetJSON() {
+	// Конструктор структуры тела запроса
+	data := &models.Data{
+		Type: "counter",
+		Name: "testCounter",
+	}
+
+	// Сериализация в JSON
+	body, _ := json.Marshal(data)
+
+	// Составление запроса с параметрами URL
+	request := httptest.NewRequest(
+		"POST",
+		"http://localhost:8080/value",
+		io.Reader(bytes.NewBuffer(body)))
+	request.Header.Add("Content-Type", "application/json")
+
+	// Создание интерфейса записи
+	w := httptest.NewRecorder()
+
+	// Регистрация нового обработчика
+	handler := NewHandler(commands)
+
+	// Выполнение запроса
+	handler.ValueGetJSON(w, request)
+
+	// Получение ответа
+	res := w.Result()
+	defer res.Body.Close()
+	resBody, _ := io.ReadAll(res.Body)
+
+	fmt.Println(res.StatusCode)
+	fmt.Println(string(resBody))
+
+	// Output:
+	// 200
+	// {"type":"counter","id":"testCounter","delta":500}
+}
+
+func ExampleHandler_ValueGet() {
+	// Составление запроса с параметрами URL
+	request := httptest.NewRequest(
+		"GET",
+		"http://localhost:8080/value",
+		nil)
+	request.Header.Add("Content-Type", "text/plain")
+	request.SetPathValue("type", "counter")
+	request.SetPathValue("name", "testCounter")
+
+	// Создание интерфейса записи
+	w := httptest.NewRecorder()
+
+	// Регистрация нового обработчика
+	handler := NewHandler(commands)
+
+	// Выполнение запроса
+	handler.ValueGet(w, request)
+
+	// Получение ответа
+	res := w.Result()
+	defer res.Body.Close()
+	resBody, _ := io.ReadAll(res.Body)
+
+	fmt.Println(res.StatusCode)
+	fmt.Println(string(resBody))
+
+	// Output:
+	// 200
+	// 500
+}
+
+func ExampleHandler_IndexGet() {
+	// Составление запроса с параметрами URL
+	request := httptest.NewRequest(
+		"GET",
+		"http://localhost:8080/",
+		nil)
+
+	// Создание интерфейса записи
+	w := httptest.NewRecorder()
+
+	// Регистрация нового обработчика
+	handler := NewHandler(commands)
+
+	// Выполнение запроса
+	handler.IndexGet(w, request)
+
+	// Получение ответа
+	res := w.Result()
+	defer res.Body.Close()
+	resBody, _ := io.ReadAll(res.Body)
+
+	fmt.Println(res.StatusCode)
+	fmt.Println(string(resBody))
+
+	// Output:
+	// 200
+	// [{"type":"counter","id":"testCounter","delta":500},{"type":"gauge","id":"testGauge","value":200},{"type":"counter","id":"alloc","delta":6502650468}]
 }
